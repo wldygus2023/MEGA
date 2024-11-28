8879e2946e432cca8262336c53931d09ed9af1d0
WINDOWS-TELNQJQ\WD <wldygus2023@gmail.com>
2024-11-28 13:13:43 +0900

    도커 생성완료 commit

--- ui/sveltekit/vite.config.ts
+++ ui/sveltekit/vite.config.ts
@@ -2,5 +2,9 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+	server: {
+		host: '0.0.0.0', // Docker 컨테이너 외부에서 접근 가능하게 설정
+		port: 3000       // UI에서 사용할 포트
+	}
 });
